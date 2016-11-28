f5643016303bfecd961df342cec4b23a3812fef6
Khan Aboubakar <khan_a@epita.fr>
2016-11-28 15:50:12 +0100

    test with require

--- test/test.js
+++ test/test.js
@@ -1,3 +1,6 @@
+var assert = require("assert");
+var todo_utils = require("../todo_utils");
+
 describe('#Error : Todo is null', function () {
    it('todo_utils._getAll should throw an error : todo is null', function () {
       todo_utils._init();
